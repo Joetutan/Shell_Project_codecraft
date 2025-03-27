027564c1483cb07aee32fcb0f82480927ef82eed
joetutan <noobianjoe@outlook.com>
2025-03-27 23:31:13 +0200

    Input_Validation

--- app/main.py
+++ app/main.py
@@ -8,6 +8,12 @@ def main():
     # Wait for user input
      input()
 
+     #read the command 
+     command = input()
+
+     #print command recieved
+     print(f"{command} : command not found")
+
 
 if __name__ == "__main__":
     main()
