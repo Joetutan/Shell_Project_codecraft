76f180b7b629b955df785d19fe9c02a2146b1613
joetutan <noobianjoe@outlook.com>
2025-03-28 21:21:21 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -2,27 +2,33 @@ import sys
 
 
 def main():
-     #Uncomment this block to pass the first stage
-     
-
-     # Wait for user input
-     #input()
-     #read the command 
+ 
      while True:
         sys.stdout.write("$ ")
+        sys.stdout.flush()
         command = input()
-        type_list = ["echo","exit", "type"]
        
-        if command[5:] in type_list:
-            print(f"{command[5:]} is a shell builtin")
-        elif command == "exit 0":
+        if command == "exit 0":
             break
-        elif command.startswith("echo "):
-            print (command[5:])
-        else:
-            print(f"{command[5:]}: not found")
-        
 
+        if command.startswith("echo "):
+              get_echo(command)
+        elif command.startswith("type "):
+              get_type(command)
+        else:
+              print(f"{command}: command not found")
+
+       
+def get_type(command):
+        type_list = ["echo","exit", "type"]
+        if command[5:] in type_list:
+                print(f"{command[5:]} is a shell builtin")
+        else:
+             print(f"{command[5:]}: not found")
+
+def get_echo(command):
+        print (command[5:])
+    
 
 
 if __name__ == "__main__":
